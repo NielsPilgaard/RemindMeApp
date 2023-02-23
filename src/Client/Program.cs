abcaa557af21e0fc5058d82b17baf447a3506df2
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-02-23 23:19:39 +0100

    update client program.cs

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,20 +1,17 @@
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RemindMeApp.Client;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+//builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("RemindMeApp.Server", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddHttpClient<AuthClient>(client =>
+    client.BaseAddress = new Uri($"{builder.HostEnvironment.BaseAddress}api/"));
 
-// Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("RemindMeApp.Server"));
-
-builder.Services.AddApiAuthorization();
+builder.Services.AddHttpClient<ReminderClient>(client =>
+    client.BaseAddress = new Uri($"{builder.HostEnvironment.BaseAddress}api/"));
 
 builder.Services.AddMudServices();
 
