099c13ff011678f928b26cceb03cb37fe2aadff9
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-02-23 22:43:31 +0100

    Update SharedClass.cs

--- src/Shared/SharedClass.cs
+++ src/Shared/SharedClass.cs
@@ -4,6 +4,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RemindMeApp.Shared;
 
+public class ReminderItem
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; } = default!;
+
+    public string? Description { get; set; }
+
+    //public NotificationType NotificationType { get; set; }
+
+    public bool IsComplete { get; set; }
+
+    [Required]
+    public DateTime ScheduleUtc { get; set; }
+}
 public class UserInfo
 {
     [Required]
